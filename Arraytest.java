f36316d0e8988972dcb39e587653ce429b17b242
Ryan Lee <chrl98@student.cis.edu.hk>
2016-01-05 17:30:45 +0800

    Preliminary version of Array population and displaying program

---
+++ Arraytest.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+import java.util.*;
+
+class ArrayTest {
+	
+	public static void main(String[] args) {
+		
+		private int [][] data = new int [10][];
+		private testArray [3] = new int[100];
+		private testArray [4][10] = 10;
+		
+		//two methods: populate array, display contents in array (pass 2D array to populate and display)
+	}
+	
+	public void populate (int[][] data)
+	{
+		Random r = new Random();
+		int row1 = r.nextInt(20);
+		
+	}
+	
+	public void display (int[][] data)
+	{
+		
+		System.out.println(testArray [4][10]);
+		
+	}
+}
