2ad826166d9a6e616b58268a31fca5b8791c2e22
Ryan Lee <chrl98@student.cis.edu.hk>
2016-02-24 23:47:19 +0800

    Bug fixes

--- hw-netbeans/rl-hw5b-swing-intro/src/VideoStoreUI_v2.java
+++ hw-netbeans/rl-hw5b-swing-intro/src/VideoStoreUI_v2.java
@@ -398,13 +398,13 @@ public class VideoStoreUI_v2 extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(VideoStoreGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(VideoStoreUI_v2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(VideoStoreGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(VideoStoreUI_v2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(VideoStoreGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(VideoStoreUI_v2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(VideoStoreGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(VideoStoreUI_v2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
