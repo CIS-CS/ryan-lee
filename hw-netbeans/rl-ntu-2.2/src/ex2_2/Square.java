ccd74c22168eaf7507a6f9b4b49ad90966161369
Ryan Lee <chrl98@student.cis.edu.hk>
2016-03-02 08:26:43 +0800

    changes

--- hw-netbeans/rl-ntu-2.2/src/ex2_2/Square.java
+++ hw-netbeans/rl-ntu-2.2/src/ex2_2/Square.java
@@ -20,7 +20,7 @@ public class Square extends Rectangle {
     }
    
     public Square (double side, String color, boolean filled){
-        super (side, side, color, filled );
+        super (side, side, color, filled);
     }
     
     public double getSide(){
